dcd0e4d3e06c17876bb627f549494a83f70e1217
shin-jung <shiro210218@gmail.com>
2020-02-07 18:01:45 +0800

    login success

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -37,7 +38,7 @@ class User extends Authenticatable implements JWTSubject
         'email_verified_at' => 'datetime',
     ];
 
-    public function getJWTIdentifer()
+    public function getJWTIdentifier()
     {
         return $this->getKey();
     }
